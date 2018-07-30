fb84603b46672292405029307dda91508cbf5bb8
89303992@qq <89303992@qq.com>
2018-07-30 17:08:13 +0800

    完善代码

--- user/models.py
+++ user/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
 class User(models.Model):
-    SEX = {
-        ('M','男'),
-        ('F','女'),
-        ('U','保密'),
-    }
-    nickname = models.CharField(max_length=32)
+    SEX = (
+        ('M', '男性'),
+        ('F', '女性'),
+        ('U', '保密'),
+    )
+
+    nickname = models.CharField(max_length=32, unique=True)
     password = models.CharField(max_length=128)
     icon = models.ImageField()
     age = models.IntegerField()
-    sex = models.CharField(max_length=8,choices=SEX)
+    sex = models.CharField(max_length=8, choices=SEX)
