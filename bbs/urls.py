2be1040722cde2bee7abf0f26ba84dc011194b42
89303992@qq <89303992@qq.com>
2018-07-30 16:14:22 +0800

    添加了user模块

--- bbs/urls.py
+++ bbs/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
+
 from post import views as post_views
 from user import views as user_views
 
@@ -25,8 +28,10 @@ urlpatterns = [
     url(r'^post/read/', post_views.read_post),
     url(r'^post/search/', post_views.search),
 
-    url(r'^/user/register/', user_views.register_user),
-    url(r'^/user/login/', user_views.login_user),
-    url(r'^/user/logout/', user_views.logout_user),
-    url(r'^/user/user_info/', user_views.user_info_user),
+    url(r'^user/register/', user_views.register),
+    url(r'^user/login/', user_views.login),
+    url(r'^user/logout/', user_views.logout),
+    url(r'^user/info/', user_views.user_info),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
